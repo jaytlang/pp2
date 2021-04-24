25f6dd43c4a1c9062b644badd11c32bfcf5b0f35
Brandon Yue <byue@mit.edu>
2021-04-24 16:58:31 -0700

    Modified netdrv code to automatically add ports given IP addresses from config file

--- netdrv/staticaddr.go
+++ netdrv/staticaddr.go
@@ -2,26 +2,19 @@ package netdrv
 
 import (
 	"errors"
-	"fmt"
 )
 
 // In lieu of dynamically updatable configuration,
 // use these lists to set up ncs
-var kvList []string = []string{"192.168.0.19:1235", "192.168.0.20:1235", "192.168.0.10:1235"}
-var rfList []string = []string{"192.168.0.19:1234", "192.168.0.20:1234", "192.168.0.10:1234"}
+var kvList []string = []string{"192.168.0.19", "192.168.0.20", "192.168.0.10"}
+var rfList []string = []string{"192.168.0.19", "192.168.0.20", "192.168.0.10"}
 
 // Update this prior to compilation to
 // set your own IP address
 var myIp = "192.168.0.19"
 
 func (c *NetConfig) GetMe() (int, error) {
-	me := myIp + ":"
-	if c.IsRaft {
-		me += fmt.Sprintf("%d", c.RaftPort)
-	} else {
-		me += fmt.Sprintf("%d", c.RaftPort)
-	}
-
+	me := myIp
 	for i, srv := range c.Servers {
 		if srv == me {
 			return i, nil
